3a912cbaaf27b970567c2109cbfe24e462017ddd
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-01-01 11:15:27 +0100

    Make a symbolic link so you don't have to specify the path to firebot

--- .github/linux-installer.sh
+++ .github/linux-installer.sh
@@ -1,10 +1,12 @@
 VERSION="$(git describe --tags $(git rev-list --tags --max-count=1))"
 DOWNLOAD_LINK="https://github.com/crowbartools/Firebot/releases/download/v5.60.0-beta2/firebot-${VERSION}-linux-x64.tar.gz"
 TEMP_FILE="/tmp/Firebot-$(git show-ref v5.60.0-beta2 | cut -d ' ' -f1)"
-INSTALLATION_FOLDER="/usr/bin/Firebot"
+INSTALLATION_FOLDER="/usr/bin/Firebotv5"
+SYMBOLIC_LINK="/usr/bin/firebot"
 curl -Lo $TEMP_FILE $DOWNLOAD_LINK # Download .tag.gz file
 sudo rm -f $INSTALLATION_FOLDER # Delete folder if it already exists (which should never happen)
 sudo mkdir $INSTALLATION_FOLDER # Create folder so tar can extract into it
 sudo tar -xzf $TEMP_FILE -C $INSTALLATION_FOLDER # Extract .tar.gz file into /tmp/Firebot
+sudo ln -s "${INSTALLATION_FOLDER}/Firebot v5" $SYMBOLIC_LINK
 rm $TEMP_FILE
 ls -la $INSTALLATION_FOLDER
