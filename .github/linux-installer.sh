593b8ac692771bd89f22c00210890ba681a7cc8e
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-01-01 10:53:33 +0100

    Extract tar.gz file

--- .github/linux-installer.sh
+++ .github/linux-installer.sh
@@ -1,5 +1,6 @@
 VERSION="$(git describe --tags $(git rev-list --tags --max-count=1))"
 DOWNLOAD_LINK="https://github.com/crowbartools/Firebot/releases/download/v5.60.0-beta2/firebot-${VERSION}-linux-x64.tar.gz"
 TEMP_FOLDER="/tmp/Firebot-$(git show-ref v5.60.0-beta2 | cut -d ' ' -f1)"
-echo $DOWNLOAD_LINK
-echo $TEMP_FOLDER
+curl -Lo $TEMP_FOLDER $DOWNLOAD_LINK
+tar -xzf $TEMP_FOLDER
+ls -la /tmp
