5a6fc9fd282427fe606d7eaa3f40244124c4caea
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-01-01 11:42:46 +0100

    Add a few echos to be able to see what the script currently does

--- .github/linux-installer.sh
+++ .github/linux-installer.sh
@@ -1,12 +1,19 @@
 VERSION="$(git describe --tags $(git rev-list --tags --max-count=1))"
 DOWNLOAD_LINK="https://github.com/crowbartools/Firebot/releases/download/v5.60.0-beta2/firebot-${VERSION}-linux-x64.tar.gz"
-TEMP_FILE="/tmp/Firebot-$(git show-ref v5.60.0-beta2 | cut -d ' ' -f1)"
+TEMP_FILE="/tmp/Firebot-$(VERSION)"
 INSTALLATION_FOLDER="/usr/bin/Firebotv5"
 SYMBOLIC_LINK="/usr/bin/firebot"
-curl -Lo $TEMP_FILE $DOWNLOAD_LINK # Download .tar.gz file
-sudo rm -f $INSTALLATION_FOLDER # Delete folder if it already exists (which should never happen)
-sudo mkdir $INSTALLATION_FOLDER # Create folder so tar can extract into it
-sudo tar -xzf $TEMP_FILE -C $INSTALLATION_FOLDER # Extract .tar.gz file into /tmp/Firebot
-sudo curl -Lo "${INSTALLATION_FOLDER}/icon.png" "https://firebot.app/firebot-logo-stroke.png" # Download icon for use in the .desktop file
-sudo ln -s "${INSTALLATION_FOLDER}/Firebot v5" $SYMBOLIC_LINK # Make symbolic link to be able to find Firebot without specifying a path (to be found in the PATH environment variable)
+echo "Downloading .tar.gz file"
+curl -Lo $TEMP_FILE $DOWNLOAD_LINK
+echo "Make sure the installation folder exists"
+sudo mkdir $INSTALLATION_FOLDER
+echo "Extract Firebot into ${INSTALLATION_FOLDER}"
+sudo tar -xzf $TEMP_FILE -C $INSTALLATION_FOLDER
+echo "Downloading icon for the .desktop file into ${INSTALLATION_FOLDER}/icon.png"
+sudo curl -Lo "${INSTALLATION_FOLDER}/icon.png" "https://firebot.app/firebot-logo-stroke.png"
+echo "Creating symbolic link to be able to omit the path to 'Firebot v5' and for the name to be just firebot"
+sudo ln -s "${INSTALLATION_FOLDER}/Firebot v5" $SYMBOLIC_LINK
+echo "Downloading .desktop file to be able to run Firebot without a terminal from a display manager like GDM"
+sudo curl -Lo "/usr/share/applications/firebot.desktop" "https://github.com/Wissididom/Firebot/raw/add-linux-installer/.github/firebot.desktop"
+echo "Deleting temporary file"
 rm $TEMP_FILE # Clean up temp file
