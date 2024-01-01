796b30a2627832f879f703bd9a1e753c375b6b99
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-01-01 10:43:23 +0100

    Output temporary destination folder

--- .github/linux-installer.sh
+++ .github/linux-installer.sh
@@ -1,2 +1,5 @@
 VERSION="$(git describe --tags $(git rev-list --tags --max-count=1))"
-echo "https://github.com/crowbartools/Firebot/releases/download/v5.60.0-beta2/firebot-${VERSION}-linux-x64.tar.gz"
+DOWNLOAD_LINK="https://github.com/crowbartools/Firebot/releases/download/v5.60.0-beta2/firebot-${VERSION}-linux-x64.tar.gz"
+TEMP_FOLDER="/tmp/Firebot-$(git show-ref v5.60.0-beta2 | cut -d ' ' -f1)"
+echo $DOWNLOAD_LINK
+echo $TEMP_FOLDER
