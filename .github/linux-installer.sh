ce1effa4bf6e8790ef7c5e48657386c01a93bcd7
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-01-01 10:35:54 +0100

    Echo generated download link

--- .github/linux-installer.sh
+++ .github/linux-installer.sh
@@ -1,1 +1,2 @@
-git describe --tags $(git rev-list --tags --max-count=1)
+VERSION="$(git describe --tags $(git rev-list --tags --max-count=1))"
+echo "https://github.com/crowbartools/Firebot/releases/download/v5.60.0-beta2/firebot-${VERSION}-linux-x64.tar.gz"
